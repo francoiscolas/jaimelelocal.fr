554d2432d833ec4cb41c29444f83d7f6c902e985
François Colas <francoiscolas@gmail.com>
2017-10-12 22:01:02 +0200

    geolocalize with IP by default

--- www/app/controllers/root_controller.rb
+++ www/app/controllers/root_controller.rb
@@ -3,16 +3,35 @@ class RootController < ApplicationController
   def index
     params.permit! # FIXME need to enhance this
 
-    @query    = params[:q]
+    @query = params[:q]
 
-    @location = params[:l].present? ? Geocoder.search(params[:l]).first : request.location
-    @location = nil if @location && (@location.latitude == 0.0 || @location.longitude == 0.0)
+    if params.key? :l
+      @location = Geocoder.search(params[:l]).first
+    else
+      if session[:user_location]
+        @location_name = session[:user_location]['name']
+        @location_coordinates = session[:user_location]['coordinates']
+      else
+        @location = request.location
+        @location = Geocoder.search('78.221.215.68').first
+        if location_is_valid? @location
+          session[:user_location] = {
+            'name' => @location.city,
+            'coordinates' => @location.coordinates
+          }
+        end
+      end
+    end
+    if location_is_valid? @location
+      @location_name = @location.city
+      @location_coordinates = @location.coordinates
+    end
 
     @distance = params[:d].to_i
     @distance = 20 if @distance == 0
 
-    if @location
-      ids = Place.near(@location.coordinates, @distance).order('distance').select('id').map(&:farm_id)
+    if @location_coordinates
+      ids = Place.near(@location_coordinates, @distance).order('distance').select('id').map(&:farm_id)
       @farms = Farm.where(:id => ids)
     else
       @farms = Farm.order(:updated_at)
@@ -34,6 +53,13 @@ class RootController < ApplicationController
   def farm
     @farm = Farm.find_by_url(params[:url]) or
       raise ActionController::RoutingError, 'Not Found'
+  end
+
+  private
+
+  def location_is_valid? location
+    logger.debug location
+    location && location.coordinates != [0.0, 0.0]
   end
 
 end
