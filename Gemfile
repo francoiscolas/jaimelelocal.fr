3c216d7ec9b8df3189844e5efa125cc210d4a4b0
François Colas <francoiscolas@gmail.com>
2017-10-23 00:31:58 +0200

    use mina for deployment

--- Gemfile
+++ Gemfile
@@ -27,15 +27,24 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger
+  # console
   gem 'byebug', platform: :mri
 end
 
 group :development do
-  # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
-  gem 'web-console'
   gem 'listen', '~> 3.0.5'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+
+  # Access an IRB console on exception pages or by using <%= console %>
+  # anywhere in the code.
+  gem 'web-console'
+
+  # Spring speeds up development by keeping your application running in the
+  # background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+
+  # Mina for deployment.
+  gem 'mina'
+  gem 'mina-puma'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+require 'mina/git'
+require 'mina/puma'
+require 'mina/rails'
+require 'mina/rbenv'
+
+set :application_name, 'cestlocal'
+set :user, 'joseph'
+set :domain, 'jaimelelocal.fr'
+set :deploy_to, '/var/www/cestlocal'
+set :repository, 'francoiscolas@bitbucket.org:francoiscolas/cestlocal.git'
+set :branch, 'master'
+set :puma_config, 'config/puma.production.rb'
+
+# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
+# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
+set :shared_dirs, fetch(:shared_dirs, []).push('public/system', 'log', 'tmp/pids', 'tmp/sockets')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
+task :remote_environment do
+  invoke :'rbenv:load'
+end
+
+task :setup do
+end
+
+desc "Deploys the current version to the server."
+task :deploy do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+    on :launch do
+      in_path(fetch(:current_path)) do
+        command %{mkdir -p tmp/}
+        command %{touch tmp/restart.txt}
+      end
+      invoke :'puma:restart'
+    end
+  end
+end
