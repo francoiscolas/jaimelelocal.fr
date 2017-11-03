4cc20a491621affbb6f4a3266beb10c63e1418a7
François Colas <francoiscolas@gmail.com>
2017-11-03 15:41:43 +0100

    switch between map and list on farm page

--- app/assets/javascripts/application/root/farm.js
+++ app/assets/javascripts/application/root/farm.js
@@ -1,103 +1,18 @@
 $(function () { if ($('body.farm').length == 1) {
 
-  if ($('body').is('.is-owner')) {
+  //
+  // Header
 
-    //
-    // Header
+  var Header = function (options) {
+    this.$el = options.$el;
+    this.$el.on('click', '.remove-banner.action-btn', _.bind(this.submitForm, this));
+    this.$el.on('change', 'input[type=file]', _.bind(this.submitForm, this));
+  };
 
-    var Header = function (options) {
-      this.$el = options.$el;
-      this.$el.on('click', '.remove-banner.action-btn', _.bind(this.submitForm, this));
-      this.$el.on('change', 'input[type=file]', _.bind(this.submitForm, this));
-    };
-
-    Header.prototype.submitForm = function () {
-      this.$el.find('form').submit();
-      return false;
-    };
-
-    //
-    // Main
-
-    new Header({$el: $('.banner')});
-
-  } else {
-
-    //
-    // Places
-
-    var Places = function (options) {
-      this.$el     = options.$el;
-      this.details = new google.maps.InfoWindow();
-      this.map     = new google.maps.Map(this.$el.get(0), {
-        zoom     : 10,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
-      this.places = [];
-
-      this.addPlaces($.parseJSON($('#places-data').text()));
-    };
-
-    Places.prototype.template = _.template(
-      '<div class="place-details">' +
-        '<h4><%= name %></h4>' +
-        '<p><%= description %></p>' +
-        '<h5>Adresse:</h5>' +
-        '<p><%= address %></p>' +
-        '<h5>Coordonnées GPS:</h5>' +
-        '<p>' +
-          '<%= (lat > 0) ? "N" : "S" %> <%= Math.abs(lat) %>°<br>' +
-          '<%= (lng > 0) ? "E" : "O" %> <%= Math.abs(lng) %>°<br>' +
-        '</p>' +
-      '</div>'
-    );
-
-    Places.prototype.$ = function (selector) {
-      return this.$el.find(selector);
-    };
-
-    Places.prototype.addPlace = function (place) {
-      var center = this.map.getCenter();
-
-      if (center)
-        center = new google.maps.LatLng(
-          center.lat() + -(center.lat() - place.lat) / 2,
-          center.lng() + -(center.lng() - place.lng) / 2
-        );
-      else
-        center = new google.maps.LatLng(place.lat, place.lng);
-      this.map.setCenter(center);
-
-      place.marker = new google.maps.Marker({
-        map     : this.map,
-        position: new google.maps.LatLng(place.lat, place.lng),
-        title   : place.name
-      });
-      google.maps.event.addListener(place.marker, 'click', _.bind(this.selectPlace, this, place));
-
-      this.places.push(place);
-    };
-
-    Places.prototype.addPlaces = function (places) {
-      _.forEach(places, _.bind(this.addPlace, this));
-    };
-
-    Places.prototype.selectPlace = function (place) {
-      place = _.clone(place);
-      place.description = place.description.replace(/\n/g, '<br>');
-
-      this.details.setContent(this.template(place));
-      this.details.open(this.map, place.marker);
-    };
-
-    //
-    // Main
-
-    $places = $('.places-map:not(.is-empty)');
-    if ($places.length)
-      new Places({$el: $places});
-
-  }
+  Header.prototype.submitForm = function () {
+    this.$el.find('form').submit();
+    return false;
+  };
 
   //
   // List
@@ -168,9 +83,96 @@ $(function () { if ($('body.farm').length == 1) {
   };
 
   //
+  // Map
+
+  var Map = function (options) {
+    this.$el     = options.$el;
+    this.details = new google.maps.InfoWindow();
+    this.map     = new google.maps.Map(this.$el.get(0), {
+      zoom     : 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    this.places = [];
+
+    this.addPlaces($.parseJSON($('#places-map-data').text()));
+  };
+
+  Map.prototype.template = _.template(
+    '<div class="place-details">' +
+      '<h4><%= name %></h4>' +
+      '<p><%= description %></p>' +
+      '<h5>Adresse:</h5>' +
+      '<p><%= address %></p>' +
+      '<h5>Coordonnées GPS:</h5>' +
+      '<p>' +
+        '<%= (lat > 0) ? "N" : "S" %> <%= Math.abs(lat) %>°<br>' +
+        '<%= (lng > 0) ? "E" : "O" %> <%= Math.abs(lng) %>°<br>' +
+      '</p>' +
+    '</div>'
+  );
+
+  Map.prototype.$ = function (selector) {
+    return this.$el.find(selector);
+  };
+
+  Map.prototype.addPlace = function (place) {
+    var center = this.map.getCenter();
+
+    if (center)
+      center = new google.maps.LatLng(
+        center.lat() + -(center.lat() - place.lat) / 2,
+        center.lng() + -(center.lng() - place.lng) / 2
+      );
+    else
+      center = new google.maps.LatLng(place.lat, place.lng);
+    this.map.setCenter(center);
+
+    place.marker = new google.maps.Marker({
+      map     : this.map,
+      position: new google.maps.LatLng(place.lat, place.lng),
+      title   : place.name
+    });
+    google.maps.event.addListener(place.marker, 'click', _.bind(this.selectPlace, this, place));
+
+    this.places.push(place);
+  };
+
+  Map.prototype.addPlaces = function (places) {
+    _.forEach(places, _.bind(this.addPlace, this));
+  };
+
+  Map.prototype.selectPlace = function (place) {
+    place = _.clone(place);
+    place.description = place.description.replace(/\n/g, '<br>');
+
+    this.details.setContent(this.template(place));
+    this.details.open(this.map, place.marker);
+  };
+
+  //
   // Main
 
-  new List({$el: $('.places')});
+  var $map = $('.places-map');
+
+  if ($('body').is('.is-owner')) {
+    new Header({$el: $('.banner')});
+  }
+
+  if ($map.length) {
+    new Map({$el: $map});
+
+    // make places tabs clickable, to switch between map and list.
+    $('button.places-tabs-title').click(function () {
+      var $button = $(this);
+
+      $('.places-tabs-panel').hide();
+      $('#' + $button.attr('data-target')).show();
+      $button.siblings().removeClass('is-active');
+      $button.addClass('is-active');
+    });
+  }
+
   new List({$el: $('.products')});
+  new List({$el: $('.places')});
 
 }});
